8cb88092675ec65e0c3918cc42fe87cc0cca100a
Pournima Pradeep Naik <43132261+Purnimanaik05@users.noreply.github.com>
2021-09-28 07:22:51 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+import hashlib
+
+c = socket.socket()
+port = 9999
+
+c.connect(('localhost',port))
+
+a = c.recv(1024).decode()
+data = c.recv(1024).decode()
+print("\n Welcome to the Network")
+
+c.close()
+
+hexcode = hashlib.sha256(a.encode()).hexdigest()
+
+if hexcode == data:
+    print("Integrity of data is maintained")
+    print(f"message:{a}")
+else:
+    print("Integrity of data is lost")
